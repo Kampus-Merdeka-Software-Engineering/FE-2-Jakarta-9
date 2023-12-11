ac249ba1497f7f89feddcbc006551bcab2a8b401
Abiyyu Dzamir Yunas <abiyunas14@gmail.com>
2023-12-11 14:02:54 +0700

    #	modified:   css/halaman-login.css #	new file:   img/orang.jpg #	new file:   js/dashboard-penyewa.js #	modified:   js/halaman-login.js #	modified:   js/halaman-registrasi.js #	new file:   public/dashboard-penyewa.html #	modified:   public/halaman-login.html #	modified:   public/halaman-registrasi.html #	modified:   public/index.html #	modified:   public/kumpulan-flashsale.html #	modified:   public/product.html

--- js/halaman-login.js
+++ js/halaman-login.js
@@ -1,15 +1,60 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Fungsi yang dipanggil ketika formulir login dikirim
-  function handleLogin(event) {
-      event.preventDefault(); // Mencegah formulir dikirim seperti biasa
-      alert('Login successful!'); // Gantilah dengan logika login sesuai kebutuhan
-  }
+  const loginForm = document.getElementById('loginForm');
 
-  // Mendapatkan elemen formulir login
-  const loginForm = document.querySelector('form');
+  loginForm.addEventListener('submit', async function (event) {
+    event.preventDefault();  // Mencegah formulir untuk melakukan submit bawaan
 
-  // Menambahkan event listener ke formulir untuk menangani login
-  if (loginForm) {
-      loginForm.addEventListener('submit', handleLogin);
-  }
+    try {
+      await login();  // Panggil fungsi login() di sini
+    } catch (error) {
+      console.error('Terjadi kesalahan saat login:', error.message);
+    }
+  });
 });
+
+async function login() {
+  
+  const usernameInput = document.getElementById('username');
+  const passwordInput = document.getElementById('password');
+  const roleInput = document.getElementById('role');
+
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+  const role = roleInput.value;
+
+  // Validasi input
+  if (!username || !password || !role) {
+    console.error('Harap lengkapi semua kolom');
+    return;
+  }
+
+  try {
+    const response = await fetch('http://localhost:7900/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password, role }),
+    });
+
+    if (!response.ok) {
+      // Handle kesalahan HTTP (contoh: 404 Not Found, 500 Internal Server Error, dll.)
+      const errorMessage = await response.json();
+      console.error(`Login gagal: ${errorMessage.message}`);
+      return;
+    }
+
+    const userData = await response.json();
+
+    // Redirect sesuai peran pengguna
+    if (userData.redirect) {
+      window.location.href = userData.redirect;
+    } else {
+      // Handle peran tidak valid
+      console.error('Peran pengguna tidak valid');
+    }
+  } catch (error) {
+    // Handle kesalahan selama proses fetch atau parsing JSON
+    console.error('Terjadi kesalahan:', error.message);
+  }
+}
--- js/halaman-registrasi.js
+++ js/halaman-registrasi.js
@@ -1,47 +1,50 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const registrationForm = document.querySelector('form');
+const registrationForm = document.getElementById('form');
 
-  registrationForm.addEventListener('submit', function (event) {
-    event.preventDefault();
+registrationForm.addEventListener('submit', async function (event) {
+  event.preventDefault();
 
-    const formData = new FormData(registrationForm);
+  const formData = new FormData(registrationForm);
 
-    const userData = {
-      username: formData.get('username'),
-      password: formData.get('password'),
-      nama: formData.get('nama'),
-      jenisKelamin: formData.get('jenisKelamin'),
-      telepon: formData.get('telepon'),
-      email: formData.get('email'),
-      role: parseInt(formData.get('role')),
-    };
+  const userData = {
+    username: formData.get('username'),
+    password: formData.get('password'),
+    nama: formData.get('nama'),
+    jenisKelamin: formData.get('jenisKelamin'),
+    telepon: formData.get('telepon'),
+    email: formData.get('email'),
+    role: parseInt(formData.get('role')),
+  };
 
-    fetch('http://localhost:7900/api/register', {
+  try {
+    const response = await fetch('http://localhost:7900/api/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(userData),
-    })
-      .then(response => response.json())
-      .then(data => {
-        // Menanggapi pesan dari server
-        alert(data.message);
+    });
 
-        // Clear formulir jika registrasi berhasil
-        if (data.message === 'Registrasi berhasil') {
-          registrationForm.reset();
+    if (!response.ok) {
+      throw new Error('Registrasi gagal. Silakan coba lagi.');
+    }
 
-          // Redirect ke halaman registrasi setelah 2 detik
-          setTimeout(function () {
-            window.location.href = 'halaman-registrasi.html';
-          }, 2000);
-        }
-      })
-      .catch(error => {
-        console.error(error);
-        // Handle kesalahan registrasi di frontend
-        alert('Registrasi gagal. Silakan coba lagi.');
-      });
-  });
+    const data = await response.json();
+
+    // Menanggapi pesan dari server
+    alert(data.message);
+
+    // Clear formulir jika registrasi berhasil
+    if (data.message === 'Registrasi berhasil') {
+      registrationForm.reset();
+
+      // Reload halaman setelah 2 detik
+      setTimeout(function () {
+        location.reload();
+      }, 2000);
+    }
+  } catch (error) {
+    console.error(error);
+    // Handle kesalahan registrasi di frontend
+    alert('Registrasi gagal. Silakan coba lagi.');
+  }
 });
